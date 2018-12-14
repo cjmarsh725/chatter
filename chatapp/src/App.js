5f39f51c423be2fd63ba350dacff6cd277d425db
Chris <cjmarsh725@gmail.com>
2018-12-14 14:51:55 -0800

    initial app layout

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -5,21 +5,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <div className="left-sidebar">Left Sidebar</div>
+        <div className="main-content">Main Content</div>
+        <div className="right-sidebar">Right Sidebar</div>
       </div>
     );
   }
